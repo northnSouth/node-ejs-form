d1e24de784fe5e3ce92dd46ed4593f933f8e27d0
northnSouth <aftitonf66@gmail.com>
2022-06-09 10:07:54 +0700

    Initial commit
    Created static html and scss(css)

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http')
+	, fs = require('fs')
+	, ejs = require('ejs')
+	, parseLang = require('accept-language-parser')
+
+http.createServer((req, res) => {
+	
+	res.setHeader('Content-Type', 'text/html')
+	
+	let path = __dirname + "/dynamic/";
+	var text;
+	switch(req.url) {
+		case '/':
+			text = "You are at Home";
+			break;
+		case '/requests':
+			text = "You are at Requests";
+			break;
+		case '/forms/1':
+			text = "You are seeing form 1"
+			break;
+		default:
+			text = "You are at 404";
+			break;
+	}
+
+	res.write(text);
+	res.end();
+
+}).listen(3000, () => {
+	console.log('Listening on port 3000');
+})
